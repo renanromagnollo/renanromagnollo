8e9d5349b5239ff96b5ed5deffe721215c77b981
Renan Romagnoli <renanromagnoli@yahoo.com.br>
2023-04-05 21:24:16 -0300

    feat: lib polished added

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,3 +1,4 @@
+import { lighten } from 'polished'
 import {createGlobalStyle} from 'styled-components'
 
 export default createGlobalStyle`
@@ -5,10 +6,39 @@ export default createGlobalStyle`
         margin: 0;
         padding: 0;
         box-sizing: border-box;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+    }
+
+    :root {
+        font-size: 62.5%;
+    }
+
+    ::-webkit-scrollbar {
+        width: 8px;
+        height: 5px;
+    }
+    ::-webkit-scrollbar-thumb {
+        background: ${({theme}) => theme.colors.primary};
+        border-radius: 10px;
+    }
+    ::-webkit-scrollbar-track {
+        background: ${({theme}) =>  lighten(0.4, theme.colors.primary)};
     }
 
     body {
         background: ${props => props.theme.colors.background};
-        color: ${props => props.theme.colors.text}
+        color: ${props => props.theme.colors.text};
+        font: ${props => props.theme.fonts.primary.font};
     }
+
+    img {
+        width: 100%;
+        max-width: 100%;
+    }
+
+    ul {
+        list-style: none;
+    }
+
 `
