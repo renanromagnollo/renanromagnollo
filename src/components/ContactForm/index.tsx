1a861b7bf1cb27cfb2408c5d550cb4b7aac1d750
Renan Romagnoli <renanromagnoli@yahoo.com.br>
2023-10-18 12:09:10 -0300

    feat: form done

--- src/components/ContactForm/index.tsx
+++ src/components/ContactForm/index.tsx
@@ -1,25 +1,48 @@
-import { Button } from "../Button";
+'use client'
+
+import { useForm } from "react-hook-form";
+import {z} from 'zod'
+import {zodResolver} from '@hookform/resolvers/zod'
+
 import {HiArrowNarrowRight} from 'react-icons/hi'
+import { Button } from "../Button";
 
-interface ContactFormProps {
+const contactFormSchema = z.object({
+    name: z.string().min(3).max(100),
+    email: z.string().email(),
+    message: z.string().min(1).max(500),
+})
 
-}
-export function ContactForm(props : ContactFormProps){
+type ContactFormData = z.infer<typeof contactFormSchema>
+
+export function ContactForm(){
+
+    const {handleSubmit, register} = useForm<ContactFormData>({
+        resolver: zodResolver(contactFormSchema)
+    })
+
+    const onSubmite = (data) => {
+
+    }
+
     return(
         <section>
             <div>
                 <h1>Contato</h1>
-                <form>
+                <form onSubmit={handleSubmit(onSubmit)}>
                     <input 
                         placeholder="Nome"
                         type="text" 
-                    />
+                        {...register('name')}
+                        />
                     <input 
                         placeholder="E-mail"
                         type="email" 
-                    />
+                        {...register('email')}
+                        />
                     <textarea 
                         placeholder="Mensagem..."
+                        {...register('message')}
                     />
                     <Button>
                         Enviar mensagem
