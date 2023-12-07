35913781558d52e67a23d0baf86cbf1d6e4e68be
Renan Romagnollo <renanromagnoli@yahoo.com.br>
2023-12-07 11:28:14 -0300

    feat: CardProfile created

--- src/components/pages/home/sections/HeroSection/CardProfile/index.tsx
+++ src/components/pages/home/sections/HeroSection/CardProfile/index.tsx
@@ -6,21 +6,78 @@ interface CardProfileProps {
 }
 
 const CardBox = styled.div`
-    width: 20vw;
+    width: 18vw;
     height: 60vh;
-    padding: 5vh;
-    background-color: ${({theme}) => theme.colors.dark.light};
+    /* padding: 5vh; */
+    /* background-color: ${({theme}) => theme.colors.dark.light}; */
     display: flex;
     flex-direction: column;
     justify-content: space-between;
     align-items: center;
-    border-radius: 5px;
+    border-radius: 8px;
+    box-shadow: 4px 4px 9px 1px rgba(0, 0, 0, 0.2);
+    border: solid 3px rgba(0, 0, 0, .3);
+    /* position: relative; */
+    overflow: hidden;
+    cursor: pointer;
+    
+
+    &:hover {
+        img {
+            filter: none
+        }
+    }
+    
+    /* &:hover {
+        box-shadow: 7px 7px 8px 2px rgba(0, 0, 0, 0.2);
+
+    } */
+
+    background-color: #4d4855;
+    background-image: linear-gradient(147deg,#3f3f3f 0%,  
+                                            #a5a5a5 4%, 
+                                            #e0e0e0 10%,
+                                            #3f3f3f 85%, 
+                                            #2e2e2e 100%);
+
 
     > img {
+
         border-radius: 50%;
         border: solid 3px ${({theme}) => theme.colors.light.dark};
-        filter: saturate(0);
-        box-shadow: 2px 2px;
+        filter: saturate(0) grayscale(20%);
+        /* box-shadow: 2px 2px 2px 1px rgba(0, 0, 0, 0.2); */
+        opacity: .8;
+        margin-top: 40px;
+        /* position: absolute; */
+        /* right: -50px; */
+        /* top: -60px; */
+        transition: all 1s ease-in 1s;
+    }
+    > div {
+        width: 100%;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-evenly;
+        align-items: flex-end;
+        padding: 20px;
+        /* background-color: pink; */
+        
+        h5 {
+            color: ${({theme}) => theme.colors.dark.light};
+        }
+
+        div {
+            display: flex;
+            flex-direction: column;
+            justify-content: space-between;
+            align-items: flex-end;
+        }
+
+        span {
+            font-size: 1.5rem;
+            color: ${({theme}) => theme.colors.dark.dark};
+        }
     }
 `
 
@@ -34,12 +91,18 @@ export function CardProfile(props : CardProfileProps){
                 sizes="100%"
                 alt="Profile Image"
                 loading="lazy"
-                style={{width: '100px', height:'100px', objectFit: 'contain'}}
+                style={{width: '120px', height:'120px', objectFit: 'contain'}}
             />
             <div>
-                <h5>brasileiro</h5>
-                <h5>37 anos</h5>
+                <h5>R. Romagnollo</h5>
+                <div>
+                    <span>renanromagnollo@gmail.com</span>
+                    <span>@renanromagnollo</span>
+                    <span>brasileiro</span>
+                    <span>37 anos</span>
+                </div>
             </div>
+            
         </CardBox>
     )
 }
