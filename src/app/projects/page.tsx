8508fdc796da3efe4eeb9085f4f952cf26c3bf1b
Renan Romagnollo <renanromagnoli@yahoo.com.br>
2023-12-08 08:19:46 -0300

    add: basic projects page added.

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,18 +1,33 @@
 'use client'
 
+import { CardContent } from "@/components/Cards/CardContent"
 import styled from "styled-components"
 
 interface ProjectsProps {
 
 }
 
-const ContainerProjects = styled.div`
+const ContainerProjects = styled.section`
+    margin: 0;
+    padding: 0;
     background-color: ${({theme}) => theme.colors.dark.dark};
+    width: 100vw;
+    display: flex;
+    flex-wrap: wrap;
+    /* grid-template-columns: 1; */
+    gap: 10px;
+
 `
 export default function Projects(props : ProjectsProps){
     return(
         <ContainerProjects>
-            <h1>Projects...</h1>
+            <CardContent/>
+            <CardContent/>
+            <CardContent/>
+            <CardContent/>
+            <CardContent/>
+            <CardContent/>
+            <CardContent/>
         </ContainerProjects>
     )
 }
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,7 +1,7 @@
 import styled from 'styled-components'
 
 export const HeaderStyle = styled.div`
-    width: 100vw;
+    width: 100%;
     height: 80px;
     display: flex;
     justify-content: space-between;
