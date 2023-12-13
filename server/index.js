27237ecbba7308d994a064c6895d198d89af3ede
Renan Romagnollo <renanromagnoli@yahoo.com.br>
2023-12-13 11:44:14 -0300

    add: jsons blogs n projects added.

--- server/index.js
+++ server/index.js
@@ -2,11 +2,15 @@ const hero = require('./hero.json');
 const tecnologies = require('./tecnologies.json')
 const techskills = require('./techskills.json')
 const socials = require('./socials.json')
+const blogs = require('./blogs.json')
+const projects = require('./projects.json')
 
 
 module.exports = () => ({
     hero,
     tecnologies,
     techskills,
-    socials
+    socials,
+    blogs,
+    projects
 })
