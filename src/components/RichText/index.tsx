57d96d370722d845b2ffc187cf59e540c92b7f75
Renan Romagnollo <renanromagnoli@yahoo.com.br>
2023-12-14 18:19:53 -0300

    feat: component RichText created

---
+++ src/components/RichText/index.tsx
@@ -0,0 +1,11 @@
+import {RichText as CMSRichText} from '@graphcms/rich-text-react-renderer'
+import {ComponentProps} from 'react'
+
+type RichTextProps = ComponentProps<typeof CMSRichText>
+
+
+export function RichText({...props} : RichTextProps){
+    return(
+        <CMSRichText {...props}/>
+    )
+}
