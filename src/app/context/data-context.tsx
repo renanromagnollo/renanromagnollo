8e007c82e51c3e77abde013596c7f9e5baca3475
Renan Romagnollo <renanromagnoli@yahoo.com.br>
2023-12-16 15:10:34 -0300

    feat: data-context created

--- src/app/context/data-context.tsx
+++ src/app/context/data-context.tsx
@@ -1,15 +1,43 @@
 import { BlogProps } from "@/types/blog-types";
 import { ProjectProps } from "@/types/project-types";
+import { ReactNode, createContext, useState } from "react";
 
 interface dataProps {
     // heros: HeroProps[]
     projects: ProjectProps[]
     blog: BlogProps[]
+    setProjects: (projects: ProjectProps[]) => void
+    setBlog: (projects: BlogProps[]) => void
 }
 
 const data: dataProps = {
     // heros: [],
     projects: [],
-    blog: []
+    blog: [],
+    setProjects: () => {},
+    setBlog: () => {} 
 }
 
+export const AppContext = createContext<dataProps>(data)
+
+function DataProvider({children}: {children: ReactNode}) {
+    
+    const [state, setState] = useState(data)
+
+    function updateState(key: string, value: any) {
+        setState({
+            ...state,
+            [key]: value
+        })
+    }
+
+    return (
+        <AppContext.Provider value={{
+            projects: state.projects,
+            blog: state.blog,
+            setProjects: projects => updateState('projects', projects),
+            setBlog: blog => updateState('blog', blog)
+        }}/>
+    )
+
+}
