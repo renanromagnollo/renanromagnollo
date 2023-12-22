e9146488ff2f4f609f8e1e7cfb91713f79369bb2
renanromagnollo <renanromagnoli@yahoo.com.br>
2023-12-22 13:53:14 -0300

    chore: setup nodejs version

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,26 +9,26 @@ import StyledComponentsRegistry from "@/styles/registry"
 
 import GlobalStyle from '../styles/global'
 
-export const metadata = {
+const metadata = {
   title: 'Renan Romagnollo',
   description: 'Front-end Developer',
 }
 
-export default function RootLayout({children,}: {children: React.ReactNode}) {
+export default function RootLayout({ children, }: { children: React.ReactNode }) {
   return (
     <html lang="pt-br">
-      <body style={{margin: '0'}}>
+      <body style={{ margin: '0' }}>
         {/* <DataProvider> */}
-          {/* <StyledComponentsRegistry> */}
-            {/* <ThemeProvider theme={dark}> */}
-            <Providers>
-              <GlobalStyle/>
-              {children}
-              <ContactForm />
+        {/* <StyledComponentsRegistry> */}
+        {/* <ThemeProvider theme={dark}> */}
+        <Providers>
+          <GlobalStyle />
+          {children}
+          <ContactForm />
 
-            </Providers>
-            {/* </ThemeProvider> */}
-          {/* </StyledComponentsRegistry> */}
+        </Providers>
+        {/* </ThemeProvider> */}
+        {/* </StyledComponentsRegistry> */}
         {/* </DataProvider> */}
       </body>
     </html>
